aa091a0d2334ff06de1015aebf339ecd902aad7b
Jacob Yepez <jacobyepez04@gmail.com>
2026-02-27 11:57:36 -0700

    Created the form button

--- sort-io.js
+++ sort-io.js
@@ -1,1 +1,10 @@
+const logItemButton = document.querySelector(".new-item");
+const itemFormPanel = document.querySelector("#item-form-panel");
+
+if (logItemButton && itemFormPanel) {
+	logItemButton.addEventListener("click", () => {
+		const isOpen = itemFormPanel.classList.toggle("is-open");
+		itemFormPanel.setAttribute("aria-hidden", String(!isOpen));
+	});
+}
 
